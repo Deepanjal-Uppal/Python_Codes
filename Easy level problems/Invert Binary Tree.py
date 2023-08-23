cfc25879fd8d7a0b479a28d0b3ad9ce94398d2f6
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2023-08-24 00:34:59 +0530

    Update and rename Invert Binary Tree to Invert Binary Tree.py

--- Easy level problems/Invert Binary Tree.py
+++ Easy level problems/Invert Binary Tree.py
@@ -1,13 +1,13 @@
-PROBLEM:
+# PROBLEM:
 
-Given the root of a binary tree, invert the tree, and return its root.
+# Given the root of a binary tree, invert the tree, and return its root.
 
-Example:
+# Example:
 
-Input: root = [4,2,7,1,3,6,9]
-Output: [4,7,2,9,6,3,1]
+# Input: root = [4,2,7,1,3,6,9]
+# Output: [4,7,2,9,6,3,1]
 
-SOLUTION:
+# SOLUTION:
 
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
