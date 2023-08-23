2d378d0a6db63cda12890944bb15fb46ca40c99f
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2023-08-24 00:42:56 +0530

    Update and rename Binary Tree Level Order Traversal to Binary Tree Level Order Traversal.py

--- Medium level problems/Binary Tree Level Order Traversal.py
+++ Medium level problems/Binary Tree Level Order Traversal.py
@@ -1,13 +1,13 @@
-PROBLEM:
+# PROBLEM:
 
-Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+# Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
 
-Example:
+# Example:
 
-Input: root = [3,9,20,null,null,15,7]
-Output: [[3],[9,20],[15,7]]
+# Input: root = [3,9,20,null,null,15,7]
+# Output: [[3],[9,20],[15,7]]
 
-SOLUTION:
+# SOLUTION:
 
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
