bc34490f014455426728c61079b79921f0b17523
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2023-08-24 00:38:29 +0530

    Update and rename Leaf Similar Trees to Leaf Similar Trees.py

--- Easy level problems/Leaf Similar Trees.py
+++ Easy level problems/Leaf Similar Trees.py
@@ -1,17 +1,17 @@
-PROBLEM:
+# PROBLEM:
 
-Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+# Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
 
-Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+# Two binary trees are considered leaf-similar if their leaf value sequence is the same.
 
-Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+# Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
 
-Example:
+# Example:
 
-Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
-Output: true
+# Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+# Output: true
 
-SOLUTION:
+# SOLUTION:
 
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
