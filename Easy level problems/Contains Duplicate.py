eeec004f09dcc96fc5808614199e4264148cf2c7
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2023-09-16 14:56:23 +0530

    Create Contains Duplicate.py

---
+++ Easy level problems/Contains Duplicate.py
@@ -0,0 +1,22 @@
+# PROBLEM:
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+
+# SOLUTION:
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        
+        return False
