155490a0df6e8cfba6abe8b7adac3f5c8f748e92
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2023-10-12 15:31:40 +0530

    Update Find in Mountain Array.py

--- Hard level problems/Find in Mountain Array.py
+++ Hard level problems/Find in Mountain Array.py
@@ -33,42 +33,46 @@
 
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        n =mountain_arr.length()
-    
-        left, right = 0, n - 1
-        while left < right:
-            mid = left + (right - left) // 2
-            if mountain_arr.get(mid) <mountain_arr.get(mid + 1):
-                left = mid + 1
+        length = mountain_arr.length()
+        l = 1
+        r = length - 2
+
+        while l <= r:
+            m = (l + r) // 2
+            left, mid, right = mountain_arr.get(m - 1), mountain_arr.get(m), mountain_arr.get(m + 1)
+            if left < mid < right:
+                l = m + 1
+            elif left > mid > right:
+                r = m - 1
             else:
-                right = mid
+                break
         
-        peak = left 
-        left, right = 0, peak
-      
-        while left <= right:
-            mid = left + (right - left) // 2
-            mid_val =mountain_arr.get(mid)
-            
-            if mid_val == target:
-                return mid
-            elif mid_val < target:
-                left = mid + 1
+        peak = m 
+
+        l = 0
+        r = peak
+
+        while l <= r:
+            m = (l + r) // 2
+            val = mountain_arr.get(m)
+            if val == target:
+                return m 
+            elif val < target:
+                l = m + 1
             else:
-                right = mid - 1
-              
-        left, right = peak, n - 1
-      
-        while left <= right:
-            mid = left + (right - left) // 2
-            mid_val =mountain_arr.get(mid)
-            
-            if mid_val == target:
-                return mid
-            elif mid_val < target:
-                right = mid - 1
-            else:
-                left = mid + 1
+                r = m - 1
         
+        l = peak
+        r = length - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            val = mountain_arr.get(m)
+            if val == target:
+                return m 
+            elif val > target:
+                l = m + 1
+            else:
+                r = m - 1
+
         return -1
-        
