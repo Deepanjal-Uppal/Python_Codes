fac829283dd0ad8ed718599b8f5b8ddecc0f4d6b
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2023-11-27 17:26:00 +0530

    Update Find the K-Beauty of a Number.py

--- Easy level problems/Find the K-Beauty of a Number.py
+++ Easy level problems/Find the K-Beauty of a Number.py
@@ -1,27 +1,27 @@
-PROBLEM:
+# PROBLEM:
 
-The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
-1. It has a length of k.
-2. It is a divisor of num.
+# The k-beauty of an integer num is defined as the number of substrings of num when it is read as a string that meet the following conditions:
+# 1. It has a length of k.
+# 2. It is a divisor of num.
 
-Given integers num and k, return the k-beauty of num.
+# Given integers num and k, return the k-beauty of num.
 
-Note:
-1. Leading zeros are allowed.
-2. 0 is not a divisor of any value.
+# Note:
+# 1. Leading zeros are allowed.
+# 2. 0 is not a divisor of any value.
 
-A substring is a contiguous sequence of characters in a string.
+# A substring is a contiguous sequence of characters in a string.
 
-Example:
+# Example:
 
-Input: num = 240, k = 2
-Output: 2
-Explanation: The following are the substrings of num of length k:
-- "24" from "240": 24 is a divisor of 240.
-- "40" from "240": 40 is a divisor of 240.
-Therefore, the k-beauty is 2.
+# Input: num = 240, k = 2
+# Output: 2
+# Explanation: The following are the substrings of num of length k:
+# - "24" from "240": 24 is a divisor of 240.
+# - "40" from "240": 40 is a divisor of 240.
+# Therefore, the k-beauty is 2.
 
-SOLUTION:
+# SOLUTION:
 
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
