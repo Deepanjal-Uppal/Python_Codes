0db7614eda7c1c2ce7d8cb2f0030d0d1443c8d66
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-02-13 16:58:28 +0530

    Create Take Gifts From the Richest Pile.py

---
+++ Easy level problems/Take Gifts From the Richest Pile.py
@@ -0,0 +1,40 @@
+# PROBLEM:
+
+# You are given an integer array gifts denoting the number of gifts in various piles. Every second, you do the following:
+
+# Choose the pile with the maximum number of gifts.
+# If there is more than one pile with the maximum number of gifts, choose any.
+# Leave behind the floor of the square root of the number of gifts in the pile. Take the rest of the gifts.
+# Return the number of gifts remaining after k seconds.
+
+# Example:
+
+# Input: gifts = [25,64,9,4,100], k = 4
+# Output: 29
+# Explanation: 
+# The gifts are taken in the following way:
+# - In the first second, the last pile is chosen and 10 gifts are left behind.
+# - Then the second pile is chosen and 8 gifts are left behind.
+# - After that the first pile is chosen and 5 gifts are left behind.
+# - Finally, the last pile is chosen again and 3 gifts are left behind.
+# The final remaining gifts are [5,8,9,4,3], so the total number of gifts remaining is 29.
+
+# SOLUTION:
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+
+        gifts = [-x for x in gifts]
+        heapq.heapify(gifts)
+        res = 0
+
+        while k > 0:
+            num = abs(heapq.heappop(gifts))
+            sq = math.floor(num**(1/2))
+            heapq.heappush(gifts, -sq)
+            k -= 1
+        
+        for i in gifts:
+            res += abs(i)
+
+        return res
