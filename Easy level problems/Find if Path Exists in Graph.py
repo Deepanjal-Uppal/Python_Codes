76cc2616e07999141f9c7677bbed0983a7f402ca
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-04-21 19:20:09 +0530

    Create Find if Path Exists in Graph.py

---
+++ Easy level problems/Find if Path Exists in Graph.py
@@ -0,0 +1,44 @@
+# PROBLEM:
+
+# There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). 
+# The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. 
+# Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+
+# You want to determine if there is a valid path that exists from vertex source to vertex destination.
+
+# Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+
+# Example:
+
+# Input: n = 3, edges = [[0,1],[1,2],[2,0]], source = 0, destination = 2
+# Output: true
+# Explanation: There are two paths from vertex 0 to vertex 2:
+# - 0 → 1 → 2
+# - 0 → 2
+
+# SOLUTION:
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        visited = set()
+        stack = [source]
+        while stack:
+            node = stack.pop()
+
+            if node == destination:
+                return True
+            
+            if node in visited:
+                continue
+
+            visited.add(node)
+            stack.extend(graph[node])
+
+        return False
