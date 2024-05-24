497f2dbbc854550fd6a2db86cfb73e47845ebc4b
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-05-24 22:09:13 +0530

    Create Palindrome Partitioning.py

---
+++ Medium level problems/Palindrome Partitioning.py
@@ -0,0 +1,36 @@
+# PROBLEM:
+
+# Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+# Example:
+
+# Input: s = "aab"
+# Output: [["a","a","b"],["aa","b"]]
+
+# SOLUTION:
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())
+                return 
+
+            for j in range(i, len(s)):
+                if self.isPalin(s, i, j):
+                    part.append(s[i : j + 1])
+                    dfs(j + 1)
+                    part.pop()
+            
+        dfs(0)
+        return res
+
+    def isPalin(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
