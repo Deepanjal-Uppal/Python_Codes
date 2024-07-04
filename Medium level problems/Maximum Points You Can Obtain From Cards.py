1cbb0d9fc5a4d2684d672972cf32014e00cf5e41
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-07-04 14:20:43 +0530

    Create Maximum Points You Can Obtain From Cards.py

---
+++ Medium level problems/Maximum Points You Can Obtain From Cards.py
@@ -0,0 +1,36 @@
+# PROBLEM:
+
+# There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+# In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+# Your score is the sum of the points of the cards you have taken.
+
+# Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+ 
+
+# Example 1:
+
+# Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+# Output: 12
+# Explanation: After the first step, your score will always be 1. 
+#              However, choosing the rightmost card first will maximize your total score. 
+#              The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+
+SOLUTION:
+
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        start = 0
+        end = len(cardPoints) - k
+        ans = sum(cardPoints[end : ])
+        res = ans
+
+        while end < len(cardPoints):
+            ans += cardPoints[start] - cardPoints[end]
+            res = max(res, ans)
+            start += 1
+            end += 1
+
+        return res
