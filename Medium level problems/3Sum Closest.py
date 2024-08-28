8281b56b9ae714d8860823b7ede708ed80a15f86
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-08-28 13:36:46 +0530

    Create 3Sum Closest.py

---
+++ Medium level problems/3Sum Closest.py
@@ -0,0 +1,35 @@
+# PROBLEM:
+
+# Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+# Return the sum of the three integers.
+
+# You may assume that each input would have exactly one solution.
+
+# Example:
+
+# Input: nums = [-1,2,1,-4], target = 1
+# Output: 2
+# Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+# SOLUTION:
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        closest = float('inf')
+        for i in range(len(nums)):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if abs(total - target) < abs(closest - target):
+                    closest = total
+                elif total < target:
+                    j += 1
+                elif total > target:
+                    k -= 1
+                else:
+                    closest = target
+                    break
+        return closest
