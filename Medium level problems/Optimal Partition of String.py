6c5b7194de3ed64092af2ac04ceb7ff6778944fc
Deepanjal-Uppal <108554919+Deepanjal-Uppal@users.noreply.github.com>
2024-11-18 16:39:02 +0530

    Create Optimal Partition of String.py

---
+++ Medium level problems/Optimal Partition of String.py
@@ -0,0 +1,30 @@
+# PROBLEM:
+
+# Given a string s, partition the string into one or more substrings such that the characters in each substring are unique. That is, no letter appears in a single substring more than once.
+
+# Return the minimum number of substrings in such a partition.
+
+# Note that each character should belong to exactly one substring in a partition.
+
+# Example:
+
+# Input: s = "abacaba"
+# Output: 4
+# Explanation:
+# Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+# It can be shown that 4 is the minimum number of substrings needed.
+
+# SOLUTION:
+
+class Solution:
+    def partitionString(self, s: str) -> int:
+        h = set()
+        res = 1
+
+        for i in s:
+            if i in h:
+                res += 1
+                h.clear()
+            h.add(i)
+
+        return res
